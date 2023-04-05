9cbc7fd1784c3c10c81fc87fe25df7b2dd10ecd8
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2023-04-05 02:06:16 +0200

    Added challenge 2

---
+++ Challenge (2)/Challenge2.js
@@ -0,0 +1,17 @@
+function majorityElement(nums) {
+    let count = 0;
+    let majority = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            majority = nums[i];
+        }
+        if (nums[i] === majority) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return majority;
+}
